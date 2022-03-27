7e0fcc5bc898a3411ff5cdff1138314e4b28e536
ruru112233 <64506346+ruru112233@users.noreply.github.com>
2022-03-27 16:07:59 +0900

    コードの整理

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,10 +1,3 @@
-// import { useState } from "react";
-// import DatePicker from "react-datepicker";
-// import moment from "moment";
-// import "react-datepicker/dist/react-datepicker.css";
-
-// import styled from "styled-components";
-
 import FullCalender from "@fullcalendar/react";
 import dayGridPlagin from "@fullcalendar/daygrid";
 import allLocales from "@fullcalendar/core/locales-all";
@@ -20,31 +13,4 @@ export const Calender = () => {
       />
     </div>
   );
-  // console.log("レンダリンされた");
-  // const parseAsMoment = (dateTimeStr) => {
-  //   return moment.utc(dateTimeStr, "YYYY-MM-DDTHH:mm:00Z", "ja").utcOffset(9);
-  // };
-  // const toUtcIso8601str = (momentInstance) => {
-  //   return momentInstance.clone().utc().format("YYYY-MM-DDTHH:mm:00Z");
-  // };
-  // const CustomInputDatePicker = () => {
-  //   const [startDate, setStartDate] = useState(toUtcIso8601str(moment()));
-  //   const handleChenge = (date) => {
-  //     setStartDate(toUtcIso8601str(moment(date)));
-  //   };
-  //   return (
-  //     <DatePicker
-  //       selected={moment(startDate).toDate()}
-  //       onChange={handleChenge}
-  //       customInput={
-  //         <button>{parseAsMoment(startDate).format("YYYY/MM/DD")}</button>
-  //       }
-  //     />
-  //   );
-  // };
-  // return (
-  //   <div>
-  //     <CustomInputDatePicker />
-  //   </div>
-  // );
 };
