a343dbf2e8705bbdb664b6bc8a6affdf11028070
ruru112233 <64506346+ruru112233@users.noreply.github.com>
2022-03-27 14:38:57 +0900

    カレンダーの修正

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,19 +1,31 @@
 import { useState } from "react";
-import DatePicker, { moment } from "react-datepicker";
+import DatePicker from "react-datepicker";
+import moment from "moment";
 import "react-datepicker/dist/react-datepicker.css";
 
 export const Calender = () => {
+  console.log("レンダリンされた");
+  const parseAsMoment = (dateTimeStr) => {
+    return moment.utc(dateTimeStr, "YYYY-MM-DDTHH:mm:00Z", "ja").utcOffset(9);
+  };
   const toUtcIso8601str = (momentInstance) => {
     return momentInstance.clone().utc().format("YYYY-MM-DDTHH:mm:00Z");
   };
   const CustomInputDatePicker = () => {
-    const initialDate = new Date();
-    const [startDate, setStartDate] = useState(initialDate);
+    const [startDate, setStartDate] = useState(toUtcIso8601str(moment()));
     const handleChenge = (date) => {
-      setStartDate(date);
+      setStartDate(toUtcIso8601str(moment(date)));
     };
 
-    return <DatePicker selected={startDate} onChange={handleChenge} />;
+    return (
+      <DatePicker
+        selected={moment(startDate).toDate()}
+        onChange={handleChenge}
+        customInput={
+          <button>{parseAsMoment(startDate).format("YYYY/MM/DD")}</button>
+        }
+      />
+    );
   };
   return (
     <div>
