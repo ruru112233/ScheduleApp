f0f3f156a5839eee17267b214f725292b2813778
ruru112233 <64506346+ruru112233@users.noreply.github.com>
2022-03-27 16:48:17 +0900

    予定追加のテスト

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -2,10 +2,22 @@ import FullCalender from "@fullcalendar/react";
 import dayGridPlagin from "@fullcalendar/daygrid";
 import allLocales from "@fullcalendar/core/locales-all";
 
+const thisMonth = () => {
+  const today = new Date();
+  return `${today.getFullYear()}-${String(today.getMonth() + 1).padStart(
+    2,
+    "0"
+  )}`;
+};
+
 export const Calender = () => {
   return (
     <div>
       <FullCalender
+        events={[
+          { title: "event 1", date: `${thisMonth()}-01` },
+          { title: "event 2", date: `${thisMonth()}-02` }
+        ]}
         locales={allLocales}
         locale="jp"
         plugins={[dayGridPlagin]}
