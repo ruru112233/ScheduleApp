baf88e4c0a3ee863d4c9d3972786006e5f33293e
ruru112233 <64506346+ruru112233@users.noreply.github.com>
2022-03-27 15:56:55 +0900

    フルスクリーンのカレンダー導入

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,37 +1,44 @@
-import { useState } from "react";
-import DatePicker from "react-datepicker";
-import moment from "moment";
-import "react-datepicker/dist/react-datepicker.css";
+// import { useState } from "react";
+// import DatePicker from "react-datepicker";
+// import moment from "moment";
+// import "react-datepicker/dist/react-datepicker.css";
 
-import styled from "styled-components";
+// import styled from "styled-components";
+
+import FullCalender from "@fullcalendar/react";
+import dayGridPlagin from "@fullcalendar/daygrid";
 
 export const Calender = () => {
-  console.log("レンダリンされた");
-  const parseAsMoment = (dateTimeStr) => {
-    return moment.utc(dateTimeStr, "YYYY-MM-DDTHH:mm:00Z", "ja").utcOffset(9);
-  };
-  const toUtcIso8601str = (momentInstance) => {
-    return momentInstance.clone().utc().format("YYYY-MM-DDTHH:mm:00Z");
-  };
-  const CustomInputDatePicker = () => {
-    const [startDate, setStartDate] = useState(toUtcIso8601str(moment()));
-    const handleChenge = (date) => {
-      setStartDate(toUtcIso8601str(moment(date)));
-    };
-
-    return (
-      <DatePicker
-        selected={moment(startDate).toDate()}
-        onChange={handleChenge}
-        customInput={
-          <button>{parseAsMoment(startDate).format("YYYY/MM/DD")}</button>
-        }
-      />
-    );
-  };
   return (
     <div>
-      <CustomInputDatePicker />
+      <FullCalender plugins={[dayGridPlagin]} initialView="dayGridMonth" />
     </div>
   );
+  // console.log("レンダリンされた");
+  // const parseAsMoment = (dateTimeStr) => {
+  //   return moment.utc(dateTimeStr, "YYYY-MM-DDTHH:mm:00Z", "ja").utcOffset(9);
+  // };
+  // const toUtcIso8601str = (momentInstance) => {
+  //   return momentInstance.clone().utc().format("YYYY-MM-DDTHH:mm:00Z");
+  // };
+  // const CustomInputDatePicker = () => {
+  //   const [startDate, setStartDate] = useState(toUtcIso8601str(moment()));
+  //   const handleChenge = (date) => {
+  //     setStartDate(toUtcIso8601str(moment(date)));
+  //   };
+  //   return (
+  //     <DatePicker
+  //       selected={moment(startDate).toDate()}
+  //       onChange={handleChenge}
+  //       customInput={
+  //         <button>{parseAsMoment(startDate).format("YYYY/MM/DD")}</button>
+  //       }
+  //     />
+  //   );
+  // };
+  // return (
+  //   <div>
+  //     <CustomInputDatePicker />
+  //   </div>
+  // );
 };
