aa1430bf9a7209e685c40359d09a7d36c999e05c
ruru112233 <64506346+ruru112233@users.noreply.github.com>
2022-03-27 14:26:31 +0900

    カレンダー機能追加

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,7 +1,23 @@
+import { useState } from "react";
+import DatePicker, { moment } from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
 export const Calender = () => {
+  const toUtcIso8601str = (momentInstance) => {
+    return momentInstance.clone().utc().format("YYYY-MM-DDTHH:mm:00Z");
+  };
+  const CustomInputDatePicker = () => {
+    const initialDate = new Date();
+    const [startDate, setStartDate] = useState(initialDate);
+    const handleChenge = (date) => {
+      setStartDate(date);
+    };
+
+    return <DatePicker selected={startDate} onChange={handleChenge} />;
+  };
   return (
     <div>
-      <p>カレンダー</p>
+      <CustomInputDatePicker />
     </div>
   );
 };
