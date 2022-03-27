a28987215df508e4a521c43284c0a7212f9ec117
ruru112233 <64506346+ruru112233@users.noreply.github.com>
2022-03-27 14:54:10 +0900

    フッターのCSS適用

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,20 @@
+import styled from "styled-components";
+
 export const FooterLine = () => {
   return (
-    <div>
-      <p>フッター</p>
-    </div>
+    <SContainer>
+      <p>　　　フッター</p>
+    </SContainer>
   );
 };
+
+const SContainer = styled.div`
+  width: 100%;
+  height: 50px;
+  border: solid 1px;
+  background-color: #0072bc;
+  text-align: left;
+  color: #fff;
+  position: absolute;
+  bottom: 0;
+`;
