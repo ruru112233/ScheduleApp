11cb8ed2e9c1e0ade2f91047a6599f5d7c2179a0
ruru112233 <64506346+ruru112233@users.noreply.github.com>
2022-03-27 16:06:53 +0900

    日本語化対応

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -7,11 +7,13 @@
 
 import FullCalender from "@fullcalendar/react";
 import dayGridPlagin from "@fullcalendar/daygrid";
+import allLocales from "@fullcalendar/core/locales-all";
 
 export const Calender = () => {
   return (
     <div>
       <FullCalender
+        locales={allLocales}
         locale="jp"
         plugins={[dayGridPlagin]}
         initialView="dayGridMonth"
