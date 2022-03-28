760ddcd3d8be6f8b9ffa42b4f53df60253a114ad
ruru112233 <64506346+ruru112233@users.noreply.github.com>
2022-03-29 07:55:43 +0900

    スケジュール登録途中

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -4,9 +4,47 @@ import timeGridPlagin from "@fullcalendar/timegrid";
 import dayGridPlagin from "@fullcalendar/daygrid";
 import allLocales from "@fullcalendar/core/locales-all";
 import interactionPlugin, { DateClickArg } from "@fullcalendar/interaction";
+import DatePicker, { registerLocale } from "react-datepicker";
+
+import { createStyles, makeStyles } from "@material-ui/core/styles";
 
 import styled from "styled-components";
 import { SchedulePopup } from "./SchedulePopup";
+
+const useStyles = makeStyles(() => {
+  createStyles({
+    cover: {
+      opacity: 0,
+      visibility: "hidden",
+      position: "fixed",
+      width: "100%",
+      height: "100%",
+      zIndex: 1000,
+      top: 0,
+      left: 0,
+      background: "rgba(0,0,0, 0.3)"
+    },
+    form: {
+      opacity: 0,
+      visibilityj: "hidden",
+      position: "fixed",
+      top: "30%",
+      left: "40%",
+      fontWeight: "bold",
+      background: "rgba(255,255,255)",
+      width: "400px",
+      height: "300px",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      zIndex: 2000
+    },
+    inView: {
+      opacity: 1,
+      visibility: "visible"
+    }
+  });
+});
 
 const thisMonth = () => {
   const today = new Date();
@@ -27,6 +65,7 @@ export const Calender = () => {
     },
     [openDialog]
   );
+
   return (
     <SContainer>
       <FullCalender
